ffeea70991e4abe5dd5dea0dcf183a401680a252
HeeJae Yu <uheej0625@gmail.com>
2025-04-30 07:59:32 +0000

    feat: add 'setting' command with LLM provider and model options

---
+++ src/bot/commands/setting.js
@@ -0,0 +1,46 @@
+import { SlashCommandBuilder, EmbedBuilder, Colors, ButtonBuilder, ButtonStyle, ActionRowBuilder, ComponentType } from 'discord.js';
+import { fs } from 'fs';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('setting')
+    .setNameLocalizations({ 
+      'ko': '설정' 
+    })
+    .setDescription('DiscordMate settings')
+    .addSubcommand(subcommand => subcommand
+      .setName('llm ')
+      .setDescription('llm settings')
+      .addStringOption(option => option
+        .setName('llm provider')
+        .setDescription('choose llm provider')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Gemini', value: 'gemini' },
+          //{ name: 'Vertex', value: 'gemini' },
+          //{ name: 'OpenAI', value: 'openai' },
+          //{ name: 'Anthropic', value: 'anthropic' },
+        )
+      )
+      .addStringOption(option => option
+        .setName('llm model')
+        .setDescription('choose llm model')
+        .setRequired(true)
+
+        // 여기에 자동완성 추가
+
+        // .addChoices(
+        //   { name: 'Gemini', value: 'gemini' },
+        //   { name: 'OpenAI', value: 'openai' },
+        //   { name: 'Anthropic', value: 'anthropic' },
+        // )
+      )
+    ),
+  async execute(interaction) {
+    const { client } = interaction
+    const user = interaction.user
+
+
+    interaction.reply('test')
+  },
+};
--- src/bot/commands/test.js
+++ src/bot/commands/test.js
@@ -3,7 +3,13 @@ import { SlashCommandBuilder, EmbedBuilder, Colors, ButtonBuilder, ButtonStyle, 
 export default {
   data: new SlashCommandBuilder()
     .setName('test')
-    .setDescription('Just test'),
+    .setNameLocalization({
+      ko: '테스트',
+    })
+    .setDescription('Just test')
+    .setDescriptionLocalization({
+      ko: '그냥 테스트',
+    }),
   async execute(interaction) {
     const { client } = interaction
     const user = interaction.user
